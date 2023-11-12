56f730abe7eb7f31a0133ac8c78170a54fa82c10
Ambe Mbong-Nwi <101456721+Ambe-Mbong-Nwi@users.noreply.github.com>
2023-11-12 09:21:12 +0100

    Add files via upload

---
+++ Projects/camnes/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/ambe/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/ambe/Documents/Flutter_Projects/Flutter_Internship/camnes" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/ambe/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/ambe/Documents/Flutter_Projects/Flutter_Internship/camnes"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jZGJlZGE3ODhhMjkzZmEyOTY2NWRjM2ZhM2Q2ZTYzYmQyMjFjYjBkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/ambe/Documents/Flutter_Projects/Flutter_Internship/camnes/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/ambe/Documents/Flutter_Projects/Flutter_Internship/camnes/lib/main.dart"
+)
